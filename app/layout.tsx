760fb5fc8a873488586d0d40fa1fbb9351db7f5c
Varote Maiderata <copterrter@Varotes-MacBook-Air.local>
2026-03-07 11:01:41 +0700

    chore: remove floating mascot due to performance overhead

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import FloatingMascot from "@/components/FloatingMascot";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -38,7 +37,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <FloatingMascot />
         {children}
       </body>
     </html>
